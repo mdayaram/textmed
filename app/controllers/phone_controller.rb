63ce7850f647cb84806d099cfb18f7f98949c4f7
Manoj Dayaram <noj@moovweb.com>
2015-03-22 21:00:32 -0700

    setting up initial sms stuff

--- app/controllers/phone_controller.rb
+++ app/controllers/phone_controller.rb
@@ -1,13 +1,16 @@
 class PhoneController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
+  # temporary controller for testing.
   def sms
-    u = User.find(param[:user])
-    sms_body = param[:sms_body]
+    u = User.find(params[:user])
+    sms_body = params[:sms_body]
     @twiclient = Twilio::REST::Client.new
     @twiclient.messages.create(
       from: ENV["TWILIO_PHONE"],
       to: u.phone_number,
       body: sms_body
     )
+    render text: "okay!"
   end
 end
