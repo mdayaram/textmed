ef4dd8384030fe53c9a84f08b2840cdc69096254
Manoj Dayaram <noj@moovweb.com>
2015-03-24 19:24:25 -0700

    only admin users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [
-    :inside
-  ]
+  before_action :authenticate_user!
 
   def home
+  	if !current_user.admin
+  		sign_out
+  	end
   	redirect_to admin_users_path
   end
 
   def inside
+  	if !current_user.admin
+  		sign_out
+  	end
   	redirect_to admin_users_path
   end
 
